1daba5761f449a9314b429aec4b0ef939cf8da1a
unknown <j.dabadie81@gmail.com>
2025-04-19 11:43:10 +0200

    feat: update Actu component

--- src/components/Actualité/Actu.jsx
+++ src/components/Actualité/Actu.jsx
@@ -1,5 +1,6 @@
 import Affiche from "../../assets/Actu/Affiche.png";
 import Presse from "../../assets/Actu/Actu-St-Mandrier.png";
+import AfficheSixFrours from "../../assets/Actu/Affiche Six Fours.jpg";
 import "./actualite.css";
 
 const Actu = () => {
@@ -8,12 +9,30 @@ const Actu = () => {
       <div className="container">
         <h1>Suivez l'actualité de Pierre Dabadie</h1>
 
+        <h3>Exposition Espace Jules de Greling (Six-Fours-Les-Plages)</h3>
+        <h4>Du 3 au 18 mai 2025</h4>
+
+        <img
+          src={AfficheSixFrours}
+          alt="Affiche Expo Six Fours Pierre Dabadie Artiste Peintre"
+          className="img"
+        />
+
+        <h4>Rencontres Printanières</h4>
+        <p>
+          Du 3 au 18 mai, l’Espace de Greling à Six‑Fours‑les‑Plages réunit
+          l’hyperréalisme aérien de Pierre Dabadie et les sculptures organiques
+          d’André Mercheyer. Dabadie crée ses œuvres en peinture au pistolet,
+          tandis que Mercheyer laisse le bois dicter sa forme sur le tour.
+          Entrée libre – vernissage vendredi 2 mai à 18 h 00.
+        </p>
+
         <h3>Exposition à la Galerie Rancilio (St-Mandrier)</h3>
         <h4>Du 8 novembre au 1er décembre 2024</h4>
 
         <img
           src={Affiche}
-          alt="Affiche St Mandrier Pierre Dabadie"
+          alt="Affiche Expo St Mandrier Pierre Dabadie Artiste Peintre"
           className="img"
         />
 
